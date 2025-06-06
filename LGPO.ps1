1662d36bab8225813e3031dbf8e962c74ff19339
DarkAlchemy15 <lovatodude@gmail.com>
2025-06-05 23:20:47 -0700

    Update LGPO.ps1

--- LGPO.ps1
+++ LGPO.ps1
@@ -1,10 +1,36 @@
-$gpos = Get-ChildItem -Directory -Path "C:\tools\Test"
+<#
+    .DESCRIPTION
+        Use LGPO.exe to import multiple Local Policies 
+    .NOTES
+        Place GPOs to import in c:\tools\GPOs
+    .SYNOPSIS
+        Use LGPO.exe to import multiple Local polices. Place LGPO.exe and GPO's to import in 'C:\tools\GPOs'
+#>
+
+If (-Not (Test-Path c:\tools\GPOs)){
+    Write-host -ForegroundColor Red "Place LGPO.exe and GPO's to import in 'C:\tools\GPOs'"
+    exit
+}
+
+$path = "C:\tools\GPOs"
+$gpos = Get-ChildItem -Directory -Path $path
+$hostname = HOSTNAME
+$date = Get-Date -Format yyyy-MM-dd_HHmm
+
+If (-Not (Test-Path -Path $($path)\LGPO.exe)){
+    Write-host -ForegroundColor Red "`nDid not find LGPO.exe"
+    Write-host "Place LGPO.exe in the same folder as the GPOs`n"
+    exit
+}
 
 $log = @()
-foreach($line in $gpos){
+$log += $hostname
+$log += $date
+
+foreach ($line in $gpos){
     $row = New-Object psobject
 
-    $gpo_name = (select-xml -Path ".\Test\$($line.Name)\bkupinfo.xml" -XPath //*).Node.GPODisplayName."#cdata-section"
+    $gpo_name = (select-xml -Path "$($path)\$($line.Name)\bkupinfo.xml" -XPath //*).Node.GPODisplayName."#cdata-section"
     $row | Add-Member -name "GPO_Name" -Value $($gpo_name)  -MemberType NoteProperty
     #(select-xml -Path ".\Test\$($line.Name)\bkupinfo.xml" -XPath //*).Node.GPODisplayName
     $row | Add-Member -name "GPO_ID" -Value $($line.Name) -MemberType NoteProperty
